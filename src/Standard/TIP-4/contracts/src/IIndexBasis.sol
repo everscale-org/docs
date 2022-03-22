eb022369fa250971ece1d0f7a27ae4dd41dbfab0
Kok Kek <kokkekpek@gmail.com>
2022-03-22 20:32:02 +0300

    feat: remove contracts from docs

--- src/Standard/TIP-4/contracts/src/IIndexBasis.sol
+++
@@ -1,6 +0,0 @@
-pragma ton-solidity >= 0.58.0;
-
-interface IIndexBasis {
-    function getInfo() external view responsible returns (address collection);
-    function destruct(address gasReceiver) external;
-}
