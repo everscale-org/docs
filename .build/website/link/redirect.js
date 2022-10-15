a14f9cc87b57df56ac810843afeb9f952f3e0889
babowsk1 <evgenii.sachko@dexpa.io>
2022-10-16 00:32:53 +0300

    Update redirect.js

--- .build/website/link/redirect.js
+++ .build/website/link/redirect.js
@@ -1,23 +1,54 @@
 module.exports = {
   redirects: [
     {
-      to: '/community/explore/wallets',
-      from: '/learn/everscale-overview/exchangewallets',
-    },
-    {
       to: '/develop/tools/everdev/command-line-interface/cpp',
       from: '/develop/tools/everdev/command-line-interface/c',
     },
     {
-      to: '/concept',
-      from: '/learn',
+      to: '/develop/client-api/js-api/ever-sdk-js',
+      from: '/develop/client-api/js-api/ever-sdk-js/samples',
+    },
+    {
+      to: '/develop/tutorial/everdev-sc',
+      from: '/develop/smart-contract/getting-started',
+    },
+    {
+      to: '/develop/tutorial/smart-digital-assets',
+      from: '/develop/smart-digital-assets',
+    },
+    {
+      to: '/gs/welcome',
+      from: '/learn/welcome',
+    },
+    {
+      to: '/ecosystem/explore/projects',
+      from: '/learn/everscale-overview/ecosystem',
     },
   ],
   createRedirects(existingPath) {
-    if (existingPath.includes('/develop/tools')) {
+    if (existingPath.includes('/ecosystem/contribute')) {
+      return [existingPath.replace('/ecosystem/contribute', '/contribute'),];
+    }
+    if (existingPath.includes('/ecosystem/explore')) {
       return [
-        existingPath.replace('/develop/tools', '/develop/api-tools'),
-      ]
+        existingPath.replace('/ecosystem/explore', '/learn/everscale-overview'),
+        existingPath.replace('/ecosystem/explore', '/learn/tutorial'),
+      ];
+    }
+    if (existingPath.includes('/learn/everscale-overview')) {
+      return [existingPath.replace('/learn/everscale-overview', '/learn'),];
+    }
+    if (existingPath.includes('/develop/tools')) {
+      return [existingPath.replace('/develop/tools', '/develop/api-tools'),];
+    }
+    if (existingPath.includes('/develop/smart-contracts')) {
+      return [existingPath.replace('/develop/smart-contracts', '/develop/smart-contract/learn'),];
+    }
+    if (existingPath.includes('/develop/tutorial/smart-digital-assets')) {
+      return [existingPath.replace('/develop/tutorial/smart-digital-assets', '/develop/smart-digital-assets'),];
+    }
+    if (existingPath.includes('/develop')) {
+      return [existingPath.replace('/develop', '/develop/smart-contract'),];
     }
     return undefined
   }
