6b139b47afa86b1c37c4de5c88ba44f978db5491
gadillacer <lenin2732372@gmail.com>
2023-06-01 17:23:27 +0700

    add remaining opcodes from tvm.tex and ever-assembler

--- src/arch/opcodes/csv_to_md.py
+++ src/arch/opcodes/csv_to_md.py
@@ -3,13 +3,13 @@ from tabulate import tabulate
 
 def csv_to_md(csv_file):
     # Read the CSV data
-    data = pd.read_csv(csv_file)
+    data = pd.read_csv(csv_file, keep_default_na=False)
     
     # Convert the data to a markdown table
     markdown_table = tabulate(data, headers='keys', tablefmt='pipe')
 
     # Write the markdown table to a file
-    with open('tvm_opcodes.md', 'w') as f:
+    with open('tvm_instructions.md', 'w') as f:
         f.write(markdown_table)
 
 # Use the function
