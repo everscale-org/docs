7438e64e4de630a1f86b633c1dae43f4a9f99889
gadillacer <lenin2732372@gmail.com>
2023-06-02 19:11:14 +0700

    add expanded column

--- src/arch/opcodes/csv_to_md.py
+++ src/arch/opcodes/csv_to_md.py
@@ -4,18 +4,25 @@ from tabulate import tabulate
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 csv_file_path = os.path.join(script_dir, "tvm_opcodes.csv")
-md_file_path = os.path.join(script_dir, "tvm_instructions.csv")
+md_file_path = os.path.join(script_dir, "tvm_instructions.md")
 
 def csv_to_md(csv_file):
-    # Read the CSV data
-    data = pd.read_csv(csv_file, keep_default_na=False)
-    
-    # Convert the data to a markdown table
-    markdown_table = tabulate(data, headers='keys', tablefmt='pipe')
+  df = pd.read_csv(csv_file, keep_default_na=False)
 
-    # Write the markdown table to a file
-    with open(md_file_path, 'w') as f:
-        f.write(markdown_table)
+  # Escape the "|" character in the DataFrame
+  df = df.replace({'\|': '\|'}, regex=True)
 
-# Use the function
+  # Add a column with expand/collapse functionality
+  df['extra_info'] = '<details><summary>Expand</summary>' + '<span>' + df[
+    'extra_col_1'] + '<br/>' + df['extra_col_2'] + '</span>' + '</details>'
+
+  # Hide the extra columns here
+  df = df.drop('extra_col_1', axis=1)
+  df = df.drop('extra_col_2', axis=1)
+
+  md_table = tabulate(df, headers='keys', tablefmt='pipe')
+
+  with open(md_file_path, 'w') as f:
+    f.write(md_table)
+
 csv_to_md(csv_file_path)
